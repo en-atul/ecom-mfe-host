6c5954617d1eb563e9aab1004babbe135f7cdce8
atul <atkr5496@gmail.com>
2026-03-25 01:52:04 +0530

    pkg update

---
+++ @mf-types/productListingApp/ProductListingWidget.d.ts
@@ -0,0 +1,2 @@
+export * from './compiled-types/ProductListingWidget';
+export { default } from './compiled-types/ProductListingWidget';
---
+++ @mf-types/productListingApp/compiled-types/ProductListingWidget.d.ts
@@ -0,0 +1,4 @@
+import React from 'react';
+declare const ProductListingWidget: React.FC;
+export default ProductListingWidget;
+//# sourceMappingURL=ProductListingWidget.d.ts.map
